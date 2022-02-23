52e7b2ce507a2d719f83151dfb2e0ed628852310
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-02-24 04:26:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
  * Created: 2022-02-23 19:55:33 UTC
  */
 
-require '/leadcloak-16rux3dxej30.php';
+require 'leadcloak-16rux3dxej30.php';
 
 // ---------------------------------------------------
 // Configuration
 
 // Set this to false if application is properly installed.
-$enableDebugging = true;
+$enableDebugging = false;
 
 // Set this to false if you won't want to log error messages
 $enableLogging = true;
