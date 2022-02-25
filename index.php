1ed1b0d47b57d69775d31760f785d549bb934896
dubaiareaa29 <96204557+dubaiareaa29@users.noreply.github.com>
2022-02-26 01:39:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/main.css">
 	<script type="text/javascript" src="script_land.js" defer></script>
- <script type="text/javascript" src="16tqmfkpxgt9.js"></script> 		
+ <script type="text/javascript" src="16tqz6yw3z30.js"></script> 		
 	</head>
 	<body>
 		<div class="wrapper">
